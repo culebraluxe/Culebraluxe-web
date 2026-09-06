530d7195e98efb260903d1cf52cc33c3225b401b
culebraluxe <culebraluxe@gmail.com>
2026-09-06 16:18:57 -0400

    FORM-EDITOR-MVI-01: export editor runtime

---
+++ ui/form-editor/index.ts
@@ -0,0 +1,17 @@
+export { FormEditorController } from './form-editor-controller'
+export {
+  ActionFormEditorSource,
+  InMemoryFormEditorSource,
+  type FormEditorSource,
+  type FormEditorSourceResult,
+} from './source'
+export {
+  isFormEditorDirty,
+  type FormEditorCreateContext,
+  type FormEditorIntentMap,
+  type FormEditorIssuedDocument,
+  type FormEditorPageModel,
+  type FormEditorSendSignatureRequest,
+  type FormEditorSendSignatureResponse,
+  type FormEditorSignatureState,
+} from './model'
