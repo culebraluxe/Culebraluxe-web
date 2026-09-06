866da5db3b8f808eeae01452213f344651ebafcb
culebraluxe <culebraluxe@gmail.com>
2026-09-06 09:27:59 -0400

    PNS-LENS-04: export P&S canonical source

--- ui/pns-lens/index.ts
+++ ui/pns-lens/index.ts
@@ -3,6 +3,14 @@ export {
   buildPnsLensModel,
   type PnsLensFieldModel,
   type PnsLensIntentMap,
+  type PnsLensListItem,
+  type PnsLensListPage,
   type PnsLensModel,
   type PnsLensOwnerFilter,
 } from './model'
+export {
+  HttpPnsLensSource,
+  type PnsLensLoadListRequest,
+  type PnsLensLoadOptions,
+  type PnsLensSource,
+} from './source'
