84dda2d040935058f87028757ce6943fbec52517
culebraluxe <culebraluxe@gmail.com>
2026-09-05 22:30:32 -0400

    SERVICE-CORE-01: export Person service experiments

---
+++ services/person/index.ts
@@ -0,0 +1,17 @@
+export { PersonEnvelopeService } from './person-service-envelope'
+export { PersonMethodService } from './person-service-methods'
+export type { PersonRepository } from './repository'
+export {
+  PERSON_OPERATIONS,
+  type AttachPersonIdentityRequest,
+  type FindPersonByIdentityRequest,
+  type GetPersonRequest,
+  type PersonDto,
+  type PersonEnvelope,
+  type PersonIdentityDto,
+  type PersonOperationName,
+  type PersonRequestByOperation,
+  type PersonResponse,
+  type PersonResponseByOperation,
+  type SetPersonDisplayNameRequest,
+} from './types'
