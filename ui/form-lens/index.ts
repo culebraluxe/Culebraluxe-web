e46f89671711dde35c189e111fe0d341c67b3d96
culebraluxe <culebraluxe@gmail.com>
2026-09-06 00:42:11 -0400

    FORM-LENS-01: export form lens architecture

---
+++ ui/form-lens/index.ts
@@ -0,0 +1,16 @@
+export { FormLensController } from './form-lens-controller'
+export { projectListingAgreement, formatPropertyAddress } from './listing-projection'
+export { HttpFormLensSource, InMemoryFormLensSource } from './source'
+export type {
+  FormLensFieldModel,
+  FormLensFieldOrigin,
+  FormLensIntentMap,
+  FormLensListItem,
+  FormLensListPage,
+  FormLensPageModel,
+} from './model'
+export type {
+  FormLensLoadListRequest,
+  FormLensLoadOptions,
+  FormLensSource,
+} from './source'
