2b32909a10e5f83685b225d82bbefba76186213d
culebraluxe <culebraluxe@gmail.com>
2026-09-05 22:04:49 -0400

    SERVICE-CORE-01: export service core

---
+++ services/core/index.ts
@@ -0,0 +1,19 @@
+export { BaseService } from './base-service'
+export { ServiceError } from './service-error'
+export { ServiceRegistry } from './service-registry'
+export type {
+  AuditPort,
+  AuthorizationPort,
+  AuthorizationRequest,
+  DomainEventPort,
+  ServiceActor,
+  ServiceAuditEvent,
+  ServiceCapability,
+  ServiceContext,
+  ServiceDescriptor,
+  ServiceDomainEvent,
+  ServiceErrorShape,
+  ServiceFailure,
+  ServiceResult,
+  ServiceSuccess,
+} from './types'
