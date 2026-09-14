0581ed3f2c1ce06df9c777402d390ff1815ed0b7
culebraluxe <culebraluxe@gmail.com>
2026-09-14 13:56:57 -0400

    feat(calendar): export calendar service

---
+++ services/calendar/index.ts
@@ -0,0 +1,8 @@
+export { CalendarService } from './calendar-service'
+export type { CalendarRepository } from './repository'
+export {
+  CALENDAR_OPERATIONS,
+  type CalendarCommandReceipt,
+  type CalendarOperationMap,
+  type CreateAppleCalendarEventRequest,
+} from './types'
