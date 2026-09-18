e9a9562123d47375a199503a156d10b01f7c1e6a
culebraluxe <culebraluxe@gmail.com>
2026-09-18 17:03:36 -0400

    lockfile-coupling fence: drop the unused import the lint gate caught (I ran typecheck but not lint before pushing)

--- workflow_app/tests/lockfile-coupling.test.ts
+++ workflow_app/tests/lockfile-coupling.test.ts
@@ -2,7 +2,7 @@ import assert from 'node:assert/strict'
 import { execFile } from 'node:child_process'
 import { mkdir, mkdtemp, rm, writeFile } from 'node:fs/promises'
 import { tmpdir } from 'node:os'
-import { dirname, join } from 'node:path'
+import { join } from 'node:path'
 import test from 'node:test'
 import { promisify } from 'node:util'
 
