acbed71391d243180262dd48b8c6c00f5c800ff0
culebraluxe <culebraluxe@gmail.com>
2026-09-22 16:15:00 -0400

    feat(seller-strategy): mount cockpit in Portal Yew app

--- rust/ui/src/yew_portal.rs
+++ rust/ui/src/yew_portal.rs
@@ -12,6 +12,7 @@ use crate::yew_views::portal_clients::{ClientRecord, Clients};
 use crate::yew_views::portal_cockpit::Cockpit;
 use crate::yew_views::portal_forms::{FormRecord, Forms};
 use crate::yew_views::portal_projects::Projects;
+use crate::yew_views::portal_seller_strategy::SellerStrategy;
 use crate::yew_views::portal_workflow_record::WorkflowRecord;
 use crate::yew_views::portal_workflows::Workflows;
 
@@ -71,6 +72,7 @@ impl Component for PortalApp {
             "forms" => html! { <Forms model={self.model.clone()} on_msg={on_msg} /> },
             "form-record" => html! { <FormRecord model={self.model.clone()} on_msg={on_msg} /> },
             "projects" => html! { <Projects model={self.model.clone()} on_msg={on_msg} /> },
+            "seller-strategy" => html! { <SellerStrategy model={self.model.clone()} on_msg={on_msg} /> },
             "workflows" => html! { <Workflows model={self.model.clone()} on_msg={on_msg} /> },
             "workflow-record" => {
                 html! { <WorkflowRecord model={self.model.clone()} on_msg={on_msg} /> }
