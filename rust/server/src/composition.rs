622d7f86061eab15ce0b9bad929adaff98b55770
culebraluxe <culebraluxe@gmail.com>
2026-09-25 17:50:04 -0400

    server: compose intake service

--- rust/server/src/composition.rs
+++ rust/server/src/composition.rs
@@ -9,6 +9,7 @@ use crate::firms::FirmService;
 use crate::flight_recorder::FlightRecorderService;
 use crate::forms::FormService;
 use crate::guide::GuideService;
+use crate::intake::IntakeService;
 use crate::issues::IssueService;
 use crate::lookup::ServiceDirectory;
 use crate::media::MediaService;
@@ -30,7 +31,7 @@ use crate::whatsapp::WhatsAppService;
 use crate::workflow_portal::WorkflowPortalService;
 use db::{
     AccountingDao, CalendarDao, ClientDao, CockpitDao, CommsDao, ContractDao, Database,
-    DealPortalDao, FirmDao, FlightRecorderDao, FormDao, GuideDao, IssueDao, MediaDao, PersonDao, ProjectDao,
+    DealPortalDao, FirmDao, FlightRecorderDao, FormDao, GuideDao, IntakeDao, IssueDao, MediaDao, PersonDao, ProjectDao,
     PropertyDao, RelationshipEvidenceDao, SecurityDao, ShowingDao, SignatureDao, SupportDiagnosticsDao, TaskDao,
     TechCockpitDao, VaultDao, WbsDao, WhatsAppDao, WorkflowPortalDao,
 };
@@ -73,6 +74,10 @@ impl CoreServices {
 
     pub fn guide(&self) -> GuideService<GuideDao> {
         GuideService::new(GuideDao::new(self.db.clone()), self.infrastructure.clone())
+    }
+
+    pub fn intake(&self) -> IntakeService<IntakeDao> {
+        IntakeService::new(IntakeDao::new(self.db.clone()), self.infrastructure.clone())
     }
 
     pub fn issues(&self) -> IssueService<IssueDao> {
