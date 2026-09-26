4534b34756babe55349b372be105d04aefce66e7
culebraluxe <culebraluxe@gmail.com>
2026-09-26 05:36:17 -0400

    style(mq): rustfmt runtime exports

--- rust/server/src/lib.rs
+++ rust/server/src/lib.rs
@@ -47,8 +47,10 @@ pub mod whatsapp;
 pub mod workflow_portal;
 
 pub use command_runtime::{CommandDispatchError, CommandDispatcher};
-pub use mq_runtime::{MqProofSubscriber, MqRuntime, MqRuntimeConfig, MqSubscriber, MqSubscriberError};
 pub use composition::CoreServices;
+pub use mq_runtime::{
+    MqProofSubscriber, MqRuntime, MqRuntimeConfig, MqSubscriber, MqSubscriberError,
+};
 pub use service_gateway::ServiceGateway;
 pub use service_harness::ServiceHarness;
 pub use service_kernel::{ServiceKernel, ServiceKernelHealth, ServiceRegistry};
